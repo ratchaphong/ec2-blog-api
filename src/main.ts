7c7e24722fc0b9eaae9d4f569dbf14266d98c84c
ratchaphong <ratchaphongc1@gamil.com>
2025-08-11 18:35:03 +0700

    log env data.

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,23 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const PORT = Number(process.env.PORT) || 4000;
+
   const app = await NestFactory.create(AppModule);
+
+  console.log(`🚀 Server listening on port ${PORT}`);
+  console.log(`📚 Swagger: /swagger`);
+  console.log(`🔗 DATABASE_URL: ${process.env.DATABASE_URL || ''}`);
 
   const config = new DocumentBuilder()
     .setTitle('Blog API')
     .setDescription('The blog API with NestJS + Prisma')
     .setVersion('1.0')
-    // .addTag('posts')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document); // <--- http://localhost:3000/swagger
+  SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
